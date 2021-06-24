cb9db8f3195b8661b21c870f0cb12805e1f23fff
Mahesh Kakde <maheshkakde100@gmail.com>
2021-06-24 16:05:54 +0530

    Leran React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Message from './components/Message'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Message />
+        {/* <Greet name='Bruce' heroName="Batman">
+          <p>This is children props</p>
+        </Greet>
+        <Greet name='Clark' heroName="SuperMan"/>
+        <button>Action</button>
+        <Greet name='Diana' heroName="WonderWomen"/>
+        <Welcome name='Bruce' heroName="Batman"/>
+        <Welcome name='Clark' heroName="SuperMan"/>
+        <Welcome name='Diana' heroName="WonderWomen"/> */}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
